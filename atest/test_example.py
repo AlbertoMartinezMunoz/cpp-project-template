908896815073308bb9a3fb058002636dac44dae7
Alberto M <traquinedes@yahoo.es>
2026-05-23 08:40:57 +0200

    ci(acceptance-test): pytest

---
+++ atest/test_example.py
@@ -0,0 +1,18 @@
+import pytest
+
+# content of test_class.py
+class TestClass:
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        print("\nSetting up resources...")
+        yield
+        print("\nTearing down resources...")
+
+    def test_one(self):
+        print("\nRunning test one...")
+        x = "this"
+        assert "h" in x
+
+    def test_sum(self):
+        print("\nRunning test sum...")
+        assert (0.1 + 0.2) == pytest.approx(0.3)
